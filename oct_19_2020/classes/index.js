c60c2bbaeeee51bb74cdb7ad5b189c7c78ab94f7
Ariel Razon <arielrazon@Ariels-MacBook-Pro.local>
2020-10-19 22:30:03 -0700

    oct_19_2020/classes: completed classes exercise

--- oct_19_2020/classes/index.js
+++ oct_19_2020/classes/index.js
@@ -1,2 +1,9 @@
-import {Player} from './player.js';
+import {Player,TennisPlayer} from './player.js';
 
+let jimbo = new TennisPlayer('jimbo','austria',34)
+
+let famousGuy = new Player('famous guy','europe')
+
+famousGuy.getFrom();
+
+jimbo.getAge();
--- oct_19_2020/classes/player.js
+++ oct_19_2020/classes/player.js
@@ -1,3 +1,20 @@
 export class Player {
+    constructor(name, country){
+        this.name = name;
+        this.country = country;
+        this.getFrom = function(){
+            console.log(`${this.name} was born in ${this.country}`);
+        }
 
+    };
 };
+
+export class TennisPlayer extends Player{
+    constructor(name,country,age){
+        super(name,country)
+        this.age=age;
+        this.getAge = function(){
+            console.log(`${name} is ${age} years old and from ${country}. They know how to play Tennis`)
+        }
+    }
+}
