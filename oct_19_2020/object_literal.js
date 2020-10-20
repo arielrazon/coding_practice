3ce64ec6509fa725ef228ab3f6af9e59aaae5d3b
Ariel Razon <arielrazon@Ariels-MacBook-Pro.local>
2020-10-19 17:42:51 -0700

    oct_19_2020/object_literal exercise completed

--- oct_19_2020/object_literal.js
+++ oct_19_2020/object_literal.js
@@ -1,5 +1,13 @@
 function addressMaker(city,state){
-
+    //if you change newAddress object syntax to:
+    const newAddress={newCity: city, newState:state};
+    //this syntax:
+    const objectLiteralAddress = {city,state};
+    //then objectLiteralAddress will look like:
+    //{city: city, state: state}
+    console.log(newAddress)
+    //you dont need to input keys, you can just put in the values you're passing into the object.
+    console.log(objectLiteralAddress);
 }
 
 addressMaker('Austin','Texas')
