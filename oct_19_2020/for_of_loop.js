cda46b5769a6c7f9eda4b52344d950f8e31361cc
Ariel Razon <arielrazon@Ariels-MacBook-Pro.local>
2020-10-19 18:02:51 -0700

    finished exercise

--- oct_19_2020/for_of_loop.js
+++ oct_19_2020/for_of_loop.js
@@ -1,3 +1,12 @@
 let incomes = [62000, 67000, 75000];
 let total = 0;
 
+
+//the below syntax states each position in the for loop will be interacted with as the 'income' variable
+for (let income of incomes){
+    console.log(income);
+    total += income;
+}
+
+
+console.log(total)
