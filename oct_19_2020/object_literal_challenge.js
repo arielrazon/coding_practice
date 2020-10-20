045309323a8f6da5835d1e36f996fa287258f299
Ariel Razon <arielrazon@Ariels-MacBook-Pro.local>
2020-10-19 17:47:40 -0700

    oct_19_2020/object_literal_challenge: completed

--- oct_19_2020/object_literal_challenge.js
+++ oct_19_2020/object_literal_challenge.js
@@ -1,10 +1,12 @@
 function addressMaker(address){
+    const {city,state} = address;
     //if you change newAddress object syntax to:
     const newAddress={
-        city:address.city,
-        state:address.state,
+        city,
+        state,
         country:'United States'
     }
+    console.log(`The new Address is in: ${newAddress.city}, ${newAddress.state}. This address is located within country: ${newAddress.country}`)
 }
 
 addressMaker({city: 'Austin', state: 'Texas'});
