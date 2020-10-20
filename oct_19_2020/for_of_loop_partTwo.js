ee04f0dd0f1a71320bb7593230f7f84c7865dca5
Ariel Razon <arielrazon@Ariels-MacBook-Pro.local>
2020-10-19 18:03:34 -0700

    part two of exercise. completed

---
+++ oct_19_2020/for_of_loop_partTwo.js
@@ -0,0 +1,8 @@
+let fullName = "Ariel Coding Learner Razon";
+
+
+//the below syntax states each letter in the string will be interacted with as the 'char' variable
+for (let char of fullName){
+    console.log(char);
+}
+
