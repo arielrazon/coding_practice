3067b7761f6737b27554562752c729d73974bcde
Ariel Razon <arielrazon@Ariels-MacBook-Pro.local>
2020-10-19 22:06:16 -0700

    exercise for includes method

---
+++ oct_19_2020/includes.js
@@ -0,0 +1,7 @@
+//includes is not supported by internet explorer
+
+//.includes will return a boolean
+
+const listIngredients = ["flour", "sugar", "eggs", "butter"];
+
+listIngredients.includes("chocolate") ? console.log("we are going to make chocolate cake") : console.log("we can't make chocolate cake");
