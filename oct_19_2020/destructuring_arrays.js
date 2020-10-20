dc36bd8637ed85b59cc2d519629f6fcf0eb07481
Ariel Razon <arielrazon@Ariels-MacBook-Pro.local>
2020-10-19 17:32:34 -0700

    oct_19_2020/destructuring_arrays: create

---
+++ oct_19_2020/destructuring_arrays.js
@@ -0,0 +1,9 @@
+let array = ['this','is','a','normal','array'];
+
+let [firstDestructuredThing] = ['this','is','a','one-item','destructured','array'];
+
+let [first,second,third] = ['how','far','can','we','take','this','?']
+
+third='newThing'
+
+console.log({firstDestructuredThing, first, second, third})
