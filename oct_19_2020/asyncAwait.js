d266723800118b781109378cd09766fca36aa843
Ariel Razon <arielrazon@Ariels-MacBook-Pro.local>
2020-10-23 16:49:25 -0700

    practiced css animations

--- oct_19_2020/asyncAwait.js
+++ oct_19_2020/asyncAwait.js
@@ -3,9 +3,13 @@ const { default: fetch } = require("node-fetch");
 const apiUrl = "https://api.chucknorris.io/jokes/random";
 
 async function getJoke(){
+   //response will accept the return of whatever fetch returns
    const response = await fetch(apiUrl);
+   //data will accept the return from the .json()ing of response
    const data = await response.json();
    console.log(data.value);
+
+   //my attempt below:
 //     let joke = new Promise((resolve,reject)=>{
 //         let data;
 //         fetch(apiUrl)
